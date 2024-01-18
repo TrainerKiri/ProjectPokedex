dfdf82186bd70ff33f75ec6daae024c9d88d324c
Pyetro Henriques <84328607+Superior00@users.noreply.github.com>
2024-01-18 16:12:04 -0300

    update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const fetchPokemons = async() => {
 
 
 const getPokemons = async(id) => {
-    const url = 'https:/pokeapi.co/api/v2/pokemon/$(id)';
+    const url = 'https://pokeapi.co/api/v2/pokemon/$(id)';
     const resp = await fetch(url)
     const data = await resp.json()
     console.log(data)
